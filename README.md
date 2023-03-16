82207777935c9bc81a0239501d57c634a6a65677
Ekyd <Ekyd777@gmail.com>
2023-03-16 12:01:45 +0300

    added solve Java stdin stdout and README.md

--- README.md
+++ README.md
@@ -4,5 +4,12 @@
 **Structure of repo**
 
 1. Date of solve the task
-2. Name of the task
+2. Name of the task with link
 3. Description to solve the task
+
+
+16.03.2023
+--
+__[Java Stdin and Stdout I](https://www.hackerrank.com/challenges/java-stdin-and-stdout-1)__
+* Simple solve without using add component of Java
+
--- src/main/java/Java_Stdin_and_Stdout_I.java
+++ src/main/java/Java_Stdin_and_Stdout_I.java
@@ -1,2 +1,19 @@
+import java.util.*;
+
+/*
+    Link to the task
+    https://www.hackerrank.com/challenges/java-stdin-and-stdout-1
+ */
 public class Java_Stdin_and_Stdout_I {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
 }
